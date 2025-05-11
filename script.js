c1954ab9ec237af5d0ee583c108eebfb1d4af285
Yam Horin <horinyam@gmail.com>
2025-05-11 21:37:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.navbar')
+fetch('/menu.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+    eval(doc.querySelector('script').textContent)
+})
+
+
